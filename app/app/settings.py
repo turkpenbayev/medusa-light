1170e76475eedcc014adfa0424b9df8bcc222f57
turkpenbayev <mr.turkpenbaev1997@gmail.com>
2022-01-06 21:40:30 +0500

    updated settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -1,7 +1,6 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
